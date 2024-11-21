7c403a4c3254f6a1caaca7c091ae6c31c084f250
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-11-21 20:10:01 +0700

    add types

--- interfaces/src/api/store_vault_server/types.rs
+++ interfaces/src/api/store_vault_server/types.rs
@@ -1,16 +1,11 @@
-use intmax2_zkp::{
-    circuits::validity::validity_pis::ValidityPublicInputs,
-    common::{
-        trees::{
-            block_hash_tree::BlockHashMerkleProof, deposit_tree::DepositMerkleProof,
-            sender_tree::SenderLeaf,
-        },
-        witness::update_witness::UpdateWitness,
-    },
-    ethereum_types::{bytes32::Bytes32, u256::U256},
+use intmax2_zkp::{ethereum_types::u256::U256, utils::poseidon_hash_out::PoseidonHashOut};
+use plonky2::{
+    field::goldilocks_field::GoldilocksField,
+    plonk::{config::PoseidonGoldilocksConfig, proof::ProofWithPublicInputs},
 };
-use plonky2::{field::goldilocks_field::GoldilocksField, plonk::config::PoseidonGoldilocksConfig};
 use serde::{Deserialize, Serialize};
+
+use crate::data::meta_data::MetaData;
 
 type F = GoldilocksField;
 type C = PoseidonGoldilocksConfig;
@@ -18,107 +13,65 @@ const D: usize = 2;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct GetBlockNumberResponse {
-    pub block_number: u32,
+pub struct SaveBalanceProofRequest {
+    pub pubkey: U256,
+    pub balance_proof: ProofWithPublicInputs<F, C, D>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct GetAccountIdQuery {
+pub struct GetBalanceProofQuery {
+    pub pubkey: U256,
+    pub block_number: u32,
+    pub private_commitment: PoseidonHashOut,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct GetBalanceProofResponse {
+    pub balance_proof: Option<ProofWithPublicInputs<F, C, D>>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SaveDataRequest {
+    pub pubkey: U256,
+    pub data: Vec<u8>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct GetUserDataQuery {
     pub pubkey: U256,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct GetAccountIdResponse {
-    pub account_id: Option<u64>,
+pub struct GetUserDataResponse {
+    pub data: Option<Vec<u8>>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct GetUpdateWitnessQuery {
+pub struct GetDataResponse {
+    pub data: Option<(MetaData, Vec<u8>)>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct GetDataQuery {
+    pub uuid: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct GetDataAllAfterQuery {
     pub pubkey: U256,
-    pub root_block_number: u32,
-    pub leaf_block_number: u32,
-    pub is_prev_account_tree: bool,
+    pub timestamp: u64,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct GetUpdateWitnessResponse {
-    pub update_witness: UpdateWitness<F, C, D>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct GetDepositIndexAndBlockNumberQuery {
-    pub deposit_hash: Bytes32,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct GetDepositIndexAndBlockNumberResponse {
-    pub deposit_index_and_block_number: Option<(u32, u32)>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct GetBlockNumberByTxTreeRootQuery {
-    pub tx_tree_root: Bytes32,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct GetBlockNumberByTxTreeRootResponse {
-    pub block_number: Option<u32>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct GetValidityPisQuery {
-    pub block_number: u32,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct GetValidityPisResponse {
-    pub validity_pis: Option<ValidityPublicInputs>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct GetSenderLeavesQuery {
-    pub block_number: u32,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct GetSenderLeavesResponse {
-    pub sender_leaves: Option<Vec<SenderLeaf>>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct GetBlockMerkleProofQuery {
-    pub root_block_number: u32,
-    pub leaf_block_number: u32,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct GetBlockMerkleProofResponse {
-    pub block_merkle_proof: BlockHashMerkleProof,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct GetDepositMerkleProofQuery {
-    pub block_number: u32,
-    pub deposit_index: u32,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct GetDepositMerkleProofResponse {
-    pub deposit_merkle_proof: DepositMerkleProof,
+pub struct GetDataAllAfterResponse {
+    pub data: Vec<(MetaData, Vec<u8>)>,
 }
