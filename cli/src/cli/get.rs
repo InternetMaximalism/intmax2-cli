fe180c6e531d00250ae8ce44c59652ac2d24af00
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-11-26 11:35:38 +0700

    wasm

--- cli/src/cli/get.rs
+++ cli/src/cli/get.rs
@@ -1,12 +1,17 @@
+use intmax2_zkp::common::signature::key_set::KeySet;
+
+use crate::cli::client::get_client;
+
 pub async fn balance(key: KeySet) -> anyhow::Result<()> {
     let client = get_client()?;
     client.sync(key).await?;
 
     let user_data = client.get_user_data(key).await?;
     let balances = user_data.balances();
+
+    println!("Balances:");
     for (i, leaf) in balances.iter() {
-        println!("Token {}: {}", i, leaf.amount);
+        println!("\t Token {}: {}", i, leaf.amount);
     }
-    println!("-----------------------------------");
     Ok(())
 }
--- cli/src/cli/mod.rs
+++ cli/src/cli/mod.rs
@@ -1,5 +1,6 @@
 pub mod client;
 pub mod deposit;
+pub mod get;
 pub mod send;
 pub mod sync;
 pub mod utils;
