a0b1cb724ea45fa41c2c4bbc72077880cf4809f7
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-12-07 14:17:37 +0900

    add log file

--- balance-prover/src/main.rs
+++ balance-prover/src/main.rs
@@ -1,16 +1,33 @@
-use std::io;
-
 use actix_cors::Cors;
 use actix_web::{middleware::Logger, web::Data, App, HttpServer};
 use balance_prover::{
     api::{api::balance_prover_scope, balance_prover::BalanceProver},
     health_check::health_check,
 };
-use intmax2_client_sdk::utils::init_logger::init_logger;
+use env_logger::fmt::Formatter;
+use log::{LevelFilter, Record};
+use std::{
+    fs::File,
+    io::{self, Write},
+};
+
+fn init_file_logger() {
+    let log_file = File::create("log.txt").expect("Unable to create log file");
+    let log_file = std::sync::Mutex::new(log_file);
+
+    env_logger::Builder::new()
+        .format(move |buf: &mut Formatter, record: &Record| {
+            let mut log_file = log_file.lock().unwrap();
+            writeln!(buf, "{}: {}", record.level(), record.args())?;
+            writeln!(log_file, "{}: {}", record.level(), record.args())
+        })
+        .filter(None, LevelFilter::Info)
+        .init();
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    init_logger();
+    init_file_logger();
 
     dotenv::dotenv().ok();
 
--- store-vault-server/src/main.rs
+++ store-vault-server/src/main.rs
@@ -1,14 +1,31 @@
 use actix_cors::Cors;
 use actix_web::{middleware::Logger, web::Data, App, HttpServer};
-use intmax2_client_sdk::utils::init_logger::init_logger;
+use env_logger::fmt::Formatter;
+use log::{LevelFilter, Record};
+use std::{fs::File, io::Write};
 use store_vault_server::{
     api::{api::store_vault_server_scope, state::State},
     health_check::health_check,
 };
 
+fn init_file_logger() {
+    let log_file = File::create("log.txt").expect("Unable to create log file");
+    let log_file = std::sync::Mutex::new(log_file);
+
+    env_logger::Builder::new()
+        .format(move |buf: &mut Formatter, record: &Record| {
+            let mut log_file = log_file.lock().unwrap();
+            writeln!(buf, "{}: {}", record.level(), record.args())?;
+            writeln!(log_file, "{}: {}", record.level(), record.args())
+        })
+        .filter(None, LevelFilter::Info)
+        .init();
+}
+
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    init_logger();
+    init_file_logger();
 
     dotenv::dotenv().ok();
 
