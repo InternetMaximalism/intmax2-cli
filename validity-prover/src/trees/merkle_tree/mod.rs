39060fe1bbf8ca33a630430cf17a3f4e63e4d41f
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-12-26 10:57:05 +0700

    unused test

--- validity-prover/src/trees/merkle_tree/mod.rs
+++ validity-prover/src/trees/merkle_tree/mod.rs
@@ -30,49 +30,12 @@ pub trait MerkleTreeClient<V: Leafable + Serialize + DeserializeOwned>:
 
 #[cfg(test)]
 mod tests {
-    use intmax2_zkp::utils::trees::merkle_tree::u64_le_bits;
-
-    use crate::trees::{merkle_tree::mock_merkle_tree::MockMerkleTree, setup_test};
+    use crate::trees::setup_test;
 
     use super::sql_merkle_tree::SqlMerkleTree;
     use crate::trees::merkle_tree::MerkleTreeClient;
 
     type V = u32;
-
-    #[tokio::test]
-    async fn test_merkle_tree() -> anyhow::Result<()> {
-        let database_url = setup_test();
-
-        let height = 10;
-        let tree = MockMerkleTree::<V>::new(height);
-
-        let timestamp0 = 0;
-        for i in 0..5 {
-            tree.update_leaf(timestamp0, i, i as u32).await?;
-        }
-        let timestamp1 = 1;
-        for i in 5..10 {
-            tree.update_leaf(timestamp1, i, i as u32).await?;
-        }
-        tree.update_leaf(timestamp1, 3, 9).await?;
-
-        let leaves0_m = tree.get_leaves(timestamp0).await?;
-        let leaves1_m = tree.get_leaves(timestamp1).await?;
-        let root0_m = tree.get_root(timestamp0).await?;
-        let root1_m = tree.get_root(timestamp1).await?;
-        let index = 6;
-        let proof1_m = tree.prove(timestamp1, index).await?;
-        let last_timestamp_m = tree.get_last_timestamp().await?;
-
-        let timestamp = 0;
-        let tree = SqlMerkleTree::<V>::new(&database_url, 0, height);
-        tree.reset().await?;
-
-        let index_bits = u64_le_bits(index, height);
-        proof1_m.verify(&6, index_bits, root1_m)?;
-
-        Ok(())
-    }
 
     #[tokio::test]
     async fn test_speed_merkle_tree() -> anyhow::Result<()> {
